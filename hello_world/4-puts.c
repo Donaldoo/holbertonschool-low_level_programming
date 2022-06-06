e46f758b0413d75f2479fe7076028c116b06e0e4
Donaldoo <donald.kumaraku@gmail.com>
2022-06-06 06:35:24 -0700

    task4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,13 @@
-/* Prints "Programming is like building a multilingual puzzle" */
-int main(){
-	int puts("Programming is like building a multilingual puzzle") 
-		return(0)
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
