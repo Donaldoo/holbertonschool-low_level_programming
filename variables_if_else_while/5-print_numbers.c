f7a6c38d242a2933f10a115fbe1395d8373aa617
Donaldoo <donald.kumaraku@gmail.com>
2022-06-07 07:06:08 -0700

    task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *  *  * main - Entry point
+ *   *   *
+ *    *    * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char l;
+
+	for (l = '0'; l <= '9'; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
