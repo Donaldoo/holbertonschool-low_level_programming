43eff86b8b5165e3bd24122d660b98a9ccac4662
Donaldoo <donald.kumaraku@gmail.com>
2022-06-09 06:10:15 -0700

    task 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - Entry point
+ * @n: int code checks
+ * Return: 0
+ */
+
+void print_to_98(int n)
+{
+	int i;
+	
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+			{
+				printf("%i, ", i);
+			}
+			else
+			{
+				printf("%i\n", i);
+			}
+		}
+	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i != 98)
+			{
+				printf("%i, ", i);
+			}
+			else
+			{
+				printf("%i\n", i);
+			}
+		}
+	}
+	else
+	{
+		printf("%d\n", n);
+	}
+}
