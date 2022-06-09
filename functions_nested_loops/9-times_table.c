7462987f0920330c8519a347fc45f040d7b84a1a
Donaldoo <donald.kumaraku@gmail.com>
2022-06-09 04:31:14 -0700

    task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -16,17 +16,29 @@ void times_table(void)
 		for (row = 0; row <= 9; row++)
 		{
 			prod = column * row;
-			if (prod <= 9)
-				_putchar(prod + '0');
-			else
+			if (prod == 0)
 			{
+				_putchar(' ');
+				_putchar(' ');
+			}
+			if (prod <= 9)
+			{
+				if (prod > 0 && prod <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(prod + '0');
+				if (row < 9)
+					_putchar(',');
+			}
+			else if (prod > 9)
+			{
+				_putchar(' ');
 				_putchar((prod / 10) + '0');
 				_putchar((prod % 10) + '0');
-			}
-			if (row < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
+				if (row < 9)
+					_putchar(',');
 			}
 		}
 		_putchar('\n');
