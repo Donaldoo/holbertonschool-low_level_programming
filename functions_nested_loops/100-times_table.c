1a567f0a8d2a699b7767afee303ecc93cbf9c554
Donaldoo <donald.kumaraku@gmail.com>
2022-06-09 16:36:05 -0700

    task 12

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,23 +1,31 @@
 #include "main.h"
 /**
- * times_table - entry point
- *
- * return: 0
+ * print_spaces - prints ' '
+ * Return: void
  */
+void print_spaces(void)
+{
+	_putchar(' ');
+	_putchar(' ');
+	_putchar(' ');
+}
 
+/**
+ * print_times_table - entry point
+ * @n: int code checks
+ * Return: void
+ */
 void print_times_table(int n)
 {
-	int column;
-	int row;
-	int mul;
+	int column, row, mul;
 
 	if (n >= 0 && n < 16)
 	{
 		for (column = 0; column <= n; column++)
 		{
-			for (row = 0; row <=n; row++)
+			for (row = 0; row <= n; row++)
 			{
-			mul = column * row;
+				mul = column * row;
 			if (row == 0)
 			{
 				_putchar('0');
@@ -25,9 +33,7 @@ void print_times_table(int n)
 			else if (mul <= 9)
 			{
 				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
+				print_spaces();
 				_putchar(mul + '0');
 			}
 			else if (mul >= 10 && mul < 100)
