3634b04f41b7be4b8b335b881efabc78570e9add
Donaldoo <donald.kumaraku@gmail.com>
2022-06-13 06:28:00 -0700

    task 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+	}
+	return (0);
+}
