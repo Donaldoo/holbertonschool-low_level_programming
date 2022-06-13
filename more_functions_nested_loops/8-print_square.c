f974c9e2ec3d9a7f6e7670d03d562c9c1ae8438d
Donaldoo <donald.kumaraku@gmail.com>
2022-06-13 06:13:10 -0700

    task 6

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_square - entry point
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	int width, height;
+
+	for (width = 0; width < size; width++)
+	{
+		for (height = 0; height < size; height++)
+			_putchar('#');
+		_putchar('\n');
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
+
