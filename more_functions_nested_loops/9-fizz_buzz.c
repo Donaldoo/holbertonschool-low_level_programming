cf8af0fee99f6584becc79d8be9369bdfc94cf10
Donaldoo <donald.kumaraku@gmail.com>
2022-06-13 15:53:42 -0700

    task 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,12 +13,12 @@ int main(void)
 	{
 		if (i % 3 != 0 && i % 5 != 0)
 			printf("%i ", i);
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
 		else if (i % 3 == 0)
 			printf("Fizz ");
 		else if (i % 5 == 0)
 			printf("Buzz ");
-		else
-			printf("FizzBuzz");
 			
 	}
 	return (0);
