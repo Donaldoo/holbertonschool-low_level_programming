ef176c225d54ba97251e874fd9ff5cdd31114741
Donaldoo <donald.kumaraku@gmail.com>
2022-06-14 11:57:24 -0700

    task 3

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <string.h>
-#include <stdio.h>
 /**
  * _strlen - entry point
  * @s: string
---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - entry point
+ * @str: string to be printed
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
