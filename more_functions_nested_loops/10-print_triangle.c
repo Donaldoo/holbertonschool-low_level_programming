51aab6cb9bb4e6f638762e809e6af93fd2ee8106
Donaldoo <donald.kumaraku@gmail.com>
2022-06-13 17:34:40 -0700

    task 10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -8,19 +8,16 @@ void print_triangle(int size)
 {
 	int width, height;
 
-	if (size > 0)
+	for (width = 1; width <= size; width++)
 	{
-		for (width = 1; width <= size; width++)
+		for (height = 1; height <= size; height++)
 		{
-			for (height = 1; height <= size; height++)
-			{
-				if (height <= (size - width))
-					_putchar(' ');
-				else
-					_putchar('#');
-			}
-			_putchar('\n');
+			if (height <= (size - width))
+				_putchar(' ');
+			else
+				_putchar('#');
 		}
+		_putchar('\n');
 	}
 	if (size <= 0)
 		_putchar('\n');
