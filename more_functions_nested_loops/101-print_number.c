0280367f490b7c85ce2b81bc143381a4d79b1a07
Donaldoo <donald.kumaraku@gmail.com>
2022-06-14 10:22:04 -0700

    task 12

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * print_number - entry point
+ *
+ * Return: 0
+ */
+void print_number(int n)
+{
+	if (n >= 0 && n <= 9)
+		_putchar(n + '0');
+	if (n > 9 && n < 100)
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	if (n > 99 && n < 1000)
+	{
+		_putchar((n / 100) + '0');
+		_putchar(((n / 10) % 10) + '0');
+		_putchar((n % 100) + '0');
+	}
+	if (n > 999 && n < 10000)
+	{
+		_putchar((n / 1000) + '0');
+		_putchar(((n / 100) % 10) + '0');
+		_putchar(((n / 10) % 100) + '0');
+		_putchar(((n % 100) % 10) + '0');
+	}
+	if (n < -9 && n > -99)
+	{
+		n *= -1;
+		_putchar('-');
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+}
