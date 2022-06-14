6b4019dc7c15f3d939b9009b46dbdbad037a95fe
Donaldoo <donald.kumaraku@gmail.com>
2022-06-14 11:41:31 -0700

    task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * _strlen - entry point
+ * @s: string
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(s);
+	return (length);
+}
