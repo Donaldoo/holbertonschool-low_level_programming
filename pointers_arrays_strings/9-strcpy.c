f237b0cc423f475e6a9f1daec18037fdc4972c73
Donaldoo <donald.kumaraku@gmail.com>
2022-06-15 07:37:43 -0700

    task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcpy - entry point
+ * @dest: pointer
+ * @src: point
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int var;
+
+	for (var = 0; src[var] != '\0'; var++)
+	{
+		dest[var] = src[var];
+	dest[var] = '\0';
+	}
+	return (dest);
+}
