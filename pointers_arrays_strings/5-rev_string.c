013150070a148ebeee4d29c87b04d2525acbe1ab
Donaldoo <donald.kumaraku@gmail.com>
2022-06-15 04:32:45 -0700

    task 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_string= entry point
+ * @s: string
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int length, rev;
+	char swapChar;
+	
+	rev = 0;
+	length = 0;
+	while (s[length] != 0)
+		length++;
+	while (rev < length)
+	{
+		length--;
+		swapChar = s[rev];
+		s[rev] = s[length];
+		s[length] = swapChar;
+		rev++;
+	}
+}
