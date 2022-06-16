0b8b2023677cc44e01c3c91a53f120d97dcd67b6
Donaldoo <donald.kumaraku@gmail.com>
2022-06-16 05:20:59 -0700

    added task

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -12,6 +12,8 @@ char *cap_string(char *s)
 	for (i = 0; s[i]; i++)
 	{
 		if (i == 0)
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				s[i + 1] = s[i + 1] - 32;
 		if (s[i] == ' ' ||
 		s[i] == '\t' ||
 		s[i] == '\n' ||
