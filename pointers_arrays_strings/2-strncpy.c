ecb303cdb8346c60268fd327714d7387834154e2
Donaldoo <donald.kumaraku@gmail.com>
2022-06-16 02:16:08 -0700

    added task

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_strncpy - entry point
+ * @dest: destination
+ * @src: string
+ * @n: int
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}
