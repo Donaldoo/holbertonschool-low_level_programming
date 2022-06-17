febef6c060ba921c0b7cda1d3e984d93ce95ad99
Donaldoo <donald.kumaraku@gmail.com>
2022-06-17 06:54:55 -0700

    added task

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_chessboard - entry point
+ * @a: 2d string
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int row;
+	int column;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (column = 0; column < 8; column++)
+			_putchar(a[row][column]);
+		_putchar('\n');
+	}
+}
