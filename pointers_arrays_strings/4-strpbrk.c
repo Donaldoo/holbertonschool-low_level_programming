e57449d93eddf06f0a70ac65e053f2585c7326a5
Donaldoo <donald.kumaraku@gmail.com>
2022-06-17 05:06:47 -0700

    added task

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *
- *
- *
- *
+ * *_strpbrk - entry point
+ * @s: pointer to string
+ * @accept: pointer to string
+ * Return: null if no byte found, else pointer to byte in s
  */
 
 char *_strpbrk(char *s, char *accept)
