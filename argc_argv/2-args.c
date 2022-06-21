157e2d92c6a6d5bce3a6cd0c3d7eb9346a7a235a
Donaldoo <donald.kumaraku@gmail.com>
2022-06-21 15:25:51 -0700

    task 2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - entry point
+ * @argc: num of args
+ * @argv: array of args
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
