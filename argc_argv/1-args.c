1a6f0c234b5d108a67ad946094e46b05fec6446f
Donaldoo <donald.kumaraku@gmail.com>
2022-06-21 15:18:21 -0700

    task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - entryu point
+ * @argc: num of args
+ * @argv: array of args
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
