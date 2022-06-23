7af1e88c12b561eb7700dfb19d7c75490f324955
Donaldoo <donald.kumaraku@gmail.com>
2022-06-23 03:41:16 -0700

    added task

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * create_array - entry point
+ * @size: array size
+ * @c: char
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i = 0;
+
+	if (size == 0)
+		return ('\0');
+	array = malloc(size * sizeof(char));
+	if (array == NULL)
+		return (NULL);
+	for ( ; i < size; i++)
+		array[i] = c;
+	return (array);
+}
