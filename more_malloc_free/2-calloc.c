d96510e9119771469daeb0d01f8b35d4009aec79
Donaldoo <donald.kumaraku@gmail.com>
2022-06-27 05:23:12 -0700

    added task

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -15,6 +15,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 
 	array = malloc(nmemb * size);
+	if (array == NULL)
+		return (NULL);
 
 	for (i = 0; i < (nmemb * size); i++)
 		array[i] = '\0';
