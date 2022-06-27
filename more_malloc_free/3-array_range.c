e8ab0160fa49126bdba10a45c981e7cb1490308f
Donaldoo <donald.kumaraku@gmail.com>
2022-06-27 05:53:56 -0700

    added task

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+
+int *array_range(int min, int max)
+{
+	int *array;
+	int i, len;
+
+	if (min > max)
+		return (NULL);
+
+	len = max - min + 1;
+	array = malloc(sizeof(int) * len);
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		array[i] = min++;
+	return (array);
+}
