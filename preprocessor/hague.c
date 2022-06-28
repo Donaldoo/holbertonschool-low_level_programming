422c5a9508642e45d315c71c06b15bcc844e54bc
Donaldoo <donald.kumaraku@gmail.com>
2022-06-28 02:31:23 -0700

    Sorted files

---
+++ preprocessor/hague.c
@@ -0,0 +1,13 @@
+#define DIT (
+#define DAH )
+#define __DAH   ++
+#define DITDAH  *
+#define DAHDIT  for
+#define DIT_DAH malloc
+#define DAH_DIT gets
+#define _DAHDIT char
+
+char _DAH_[] = "ETIANMSURWDKGOHVFaLaPJBXCYZQb54a3d2f16g7c8a90l?e'b.s;i,d:"
+main()
+{
+	char *_DIT, *DAH_, *DIT_, *_DIT_, *malloc(), *gets();
