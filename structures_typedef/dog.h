7ac16b7994e8cec9396c021272c2995e30a5d05e
Donaldoo <donald.kumaraku@gmail.com>
2022-06-28 14:09:33 -0700

    Added task

---
+++ structures_typedef/dog.h
@@ -0,0 +1,35 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - data type describing a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - new name for struct dog
+ */
+
+typedef struct dog dog_t;
+
+
+/**
+ * Prototypes
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+
+#endif
