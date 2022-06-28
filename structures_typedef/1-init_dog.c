d37e1fb02df5e0705e8738f07a426ceb2d36cef4
Donaldoo <donald.kumaraku@gmail.com>
2022-06-28 14:25:12 -0700

    Fixed NULL case

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,13 +1,19 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
- *
- *
- *
+ * init_dog - entry point
+ * @d: dog string to be initialized
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	(*d).name = name;
-	(*d).age = age;
-	(*d).owner = owner;
+	if (d != NULL)
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
 }
