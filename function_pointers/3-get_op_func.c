142506f2f4c6e748248106e93b733abb981688cb
Donaldoo <donald.kumaraku@gmail.com>
2022-06-30 02:37:53 -0700

    Added tasks

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,27 @@
+#include "calc.h"
+#include <stdlib.h>
+/**
+ * get_op_func - entry point
+ * @s: str
+ * Return: null
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	for (i = 0; ops[i].op; i++)
+	{
+		if (*s == *ops[i].op)
+			return (ops[i].f);
+	}
+	return (NULL);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "calc.h"
+#include <stdlib.h>
+
+/**
+ * op_add - entry point
+ * @a: int 1
+ * @b: int 2
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - entry point
+ * @a: int 1
+ * @b: int 2
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - entry point
+ * @a: int 1
+ * @b: int 2
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - entry point
+ * @a: int 1
+ * @b: int 2
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - entry point
+ * @a: int 1
+ * @b: int 2
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
