8e8b5ee7893d72c6dc7c6cde0a855a564f8eade7
Donaldoo <donald.kumaraku@gmail.com>
2022-06-30 01:45:40 -0700

    Added task

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * int_index - entry point
+ * @array: array to check
+ * @size: array size
+ * @cmp: function pointer
+ * Return: -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
