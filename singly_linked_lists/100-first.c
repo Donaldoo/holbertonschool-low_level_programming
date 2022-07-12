ef580caa60063cb9748d96612ad980f71f99423a
Donaldoo <donald.kumaraku@gmail.com>
2022-07-12 09:55:44 -0700

    Added task

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- * before_main - entry point to construct attribute
- * Return: void
+ * before_main - entry point
  */
-void before_main(void) __attribute__((constructor));
+
 void before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
