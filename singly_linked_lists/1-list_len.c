492861e5fa8d9040e29bdf08f34343cd2b333057
Donaldoo <donald.kumaraku@gmail.com>
2022-07-12 08:22:07 -0700

    Added task

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len = entry point
+ * @h: linked list
+ * Return: elements number
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0;
+
+	while (h != NULL)
+	{
+		elements++;
+		h = h->next;
+	}
+	return (elements);
+}
