53ab6d869f92d61d07a6862ecdc09a7ecf946483
Donaldoo <donald.kumaraku@gmail.com>
2022-07-13 12:56:01 -0700

    Added task

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -1,10 +1,10 @@
 #include"lists.h"
 
 /**
- *
- *
- *
- *
+ * add_nodeint - func to add a new node at the beginning of a list
+ * @head: ptr to address of list's head
+ * @n: int new node contains
+ * Return: NULL if func fails, add of new element if not
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
