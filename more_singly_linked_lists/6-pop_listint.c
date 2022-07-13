e93c267a4146fd0f747d3d90e345c2faceea3615
Donaldoo <donald.kumaraku@gmail.com>
2022-07-13 14:04:31 -0700

    Added task

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- *
- *
- *
+ * pop_listint - deletes the head node
+ * @head: pr to add of list's head node
+ * Return: head node's data, null if list is empty
  */
 int pop_listint(listint_t **head)
 {
