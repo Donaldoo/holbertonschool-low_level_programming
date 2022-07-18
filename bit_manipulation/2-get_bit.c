bb0eb5ecc29546abd1f52df84cc73e7437365b15
Donaldoo <donald.kumaraku@gmail.com>
2022-07-18 08:09:50 -0700

    Added task

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_bit - returns the value of a bit at a given index 
+ * get_bit - returns the value of a bit at a given index
  * @n: bit
  * @index: index of the bit to get the value at
  * Return: the value of the bit
