975c01aede338154f4ace648bbdec29c190f6626
Donaldoo <donald.kumaraku@gmail.com>
2022-07-18 08:46:32 -0700

    Added task

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the bits needed to be flipper to get from one number to another
+ * @n: number to be flipped
+ * @m: number to flip n to
+ * Return: bits needed to flip n to m
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int r = n ^ m, bits = 0;
+
+	while (r != 0)
+	{
+		bits += r & 1;
+		r = r >> 1;
+	}
+	return (bits);
+}
