3f90cc83e704a0a4b24c39c9c44e5c4e5d1040d6
Donaldoo <donald.kumaraku@gmail.com>
2022-07-19 09:51:02 -0700

    Added task

---
+++ file_io/3-cp.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - copies the content of a file to another file
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, c;
+	ssize_t w, length;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	file_from = open(argv[1], O_RDONLY);
+	length = read(file_from, buffer, 1024);
+	if (length == -1)
+	{
+		dprintf(STDERR_FILENO,
+		"Error: Can't read from file NAME_OF_THE_FILE%s\n", argv[1]);
+		exit(98);
+	}
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while (length != '\0')
+	{
+		w = write(file_to, buffer, length);
+		if (file_from == -1 || w != length)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+
+	c = close(file_from);
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	c = close(file_to);
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
+
+	return (0);
+}
