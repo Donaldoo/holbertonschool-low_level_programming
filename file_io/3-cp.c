19db29254801baf607bf1c0baad2f9ec4a46e70b
Donaldoo <donald.kumaraku@gmail.com>
2022-07-19 10:02:22 -0700

    Added task

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 	if (length == -1)
 	{
 		dprintf(STDERR_FILENO,
-		"Error: Can't read from file file_from%s\n", argv[1]);
+		"Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
