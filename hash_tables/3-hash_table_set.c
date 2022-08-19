7a32ba85819da34386fe8889e6dfa0f9a517aa7b
Donaldoo <donald.kumaraku@gmail.com>
2022-08-19 03:16:41 -0700

    Fixed case when key is not empty

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -41,7 +41,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	new->next = ht->array[index];
-	ht->array[index] = tmp;
+	ht->array[index] = new;
 
 
 	return (1);
