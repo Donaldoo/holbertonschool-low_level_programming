5762ee78aa917fce99d156c27f1d2830b2bc4605
Donaldoo <donald.kumaraku@gmail.com>
2022-08-19 03:14:31 -0700

    Fixed case when key is not empty

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -24,10 +24,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(tmp->key, key) == 0)
 		{
-			free(tmp->value);
 			tmp->value = strdup(value);
-			if (tmp->value == NULL)
-				return (0);
 			return (1);
 		}
 		tmp = tmp->next;
