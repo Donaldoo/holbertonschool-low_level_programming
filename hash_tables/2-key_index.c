96858f26f7078bacff6ba5718b66e471f17a788b
Donaldoo <donald.kumaraku@gmail.com>
2022-08-19 02:06:09 -0700

    Added task 2

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gices the index of a key
+ * @key: key
+ * @size: size of the hash table
+ * Return: the index at which the key should be stores
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+
+	index = hash_djb2(key);
+
+	index = index % size;
+
+	return (index);
+}
